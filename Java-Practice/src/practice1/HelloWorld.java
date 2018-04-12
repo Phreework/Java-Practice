11f070d296b2a6ff9836e6b46525d7a8c0bfeab5
Phreework <phreework@gmail.com>
2018-04-12 22:05:53 +0800

    Create two new demo
    Namedemo.java record how to give name to class,variable,blabla...
    Inputdemo.java test how to input from keyboard.

---
+++ Java-Practice/src/practice1/HelloWorld.java
@@ -0,0 +1,12 @@
+package practice1;
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		print();
+	}
+
+	private static void print() {
+		System.out.println("phree's hello world!");
+	}
+
+}
