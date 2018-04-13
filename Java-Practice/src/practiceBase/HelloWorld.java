3e6821c254fa1bb9b23c7f80095b805aec575c89
Phreework <phreework@gmail.com>
2018-04-13 12:22:51 +0800

    finish base practice

---
+++ Java-Practice/src/practiceBase/HelloWorld.java
@@ -0,0 +1,12 @@
+package practiceBase;
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		print();
+	}
+
+	private static void print() {
+		System.out.println("phree's hello world!");
+	}
+
+}
