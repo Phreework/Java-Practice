05486c841c924a8cef63fea239fe13a07c760ec9
Phreework <781772991@qq.com>
2018-04-13 19:54:04 +0800

    Add StaticDemo
    use to explain what is static

---
+++ Java-Practice/src/com/practice/OOP/Student.java
@@ -0,0 +1,32 @@
+package com.practice.OOP;
+
+
+
+public class Student {
+	private String name;
+
+	private int age;
+
+	private int id;
+
+	public Student(String name, int age, int id) {
+		this.name = name;
+		this.age = age;
+		this.id = id;
+	}
+
+	public void study() {
+		System.out.println("学生"+name+"爱学习");
+	}
+
+	// 吃饭的方法
+	public void eat() {
+		System.out.println("学生"+name+"要吃饭");
+	}
+
+	// 睡觉的方法
+	public void sleep() {
+		System.out.println("学生"+name+"想睡觉");
+	}
+
+}
---
+++ Java-Practice/src/com/practice/base/InputDemo.java
@@ -0,0 +1,23 @@
+package com.practice.base;
+
+/*
+	这是测试键盘输入数据的demo
+	-------------------------
+	导入scanner包用scanner类实现数据输入
+ */
+import java.util.Scanner;
+public class InputDemo {
+	public static void main(String argsp[]){
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("请输入第一个数字：");
+		int a = sc.nextInt();
+		System.out.println("请输入第二个数字：");
+		int b = sc.nextInt();
+		
+		System.out.println("a = "+a);
+		System.out.println("b = "+b);
+		boolean c = a==b;
+		System.out.println(c);
+	}
+}
